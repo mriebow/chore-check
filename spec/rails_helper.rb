6324c7f71aa5fa5c104add270f10e76934306e42
mriebow <matthewriebow@gmail.com>
2017-05-01 21:33:55 -0400

    First commit

---
+++ spec/rails_helper.rb
@@ -0,0 +1,46 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'devise'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
+require "capybara/rails"
+require "valid_attribute"
+
+RSpec.configure do |config|
+  # For Devise <= 4.1.0
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  # Use the following instead if you are on Devise >= 4.1.1
+  # config.include Devise::TestHelpers, :type => :controller
+end
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  # This block must be here, do not combine with the other `before(:each)`block.
+  # This makes it so Capybara can see the database.
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
