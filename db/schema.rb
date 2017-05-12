ec85b583012025f0381ed808a1448b233fca8fd5
mriebow <matthewriebow@gmail.com>
2017-05-12 14:45:55 -0400

    Added styling and readme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511203940) do
+ActiveRecord::Schema.define(version: 20170512151451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170511203940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "creator_id", null: false
+    t.string   "picture"
     t.index ["creator_id"], name: "index_groups_on_creator_id", using: :btree
   end
 
