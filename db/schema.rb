b74216cb745e55bffeadc3fd68fc25cede5869f2
mriebow <matthewriebow@gmail.com>
2017-05-12 14:53:06 -0400

    Added styling and readme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512151451) do
+ActiveRecord::Schema.define(version: 20170512172337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20170512151451) do
     t.datetime "updated_at",                             null: false
     t.string   "username",               default: "",    null: false
     t.boolean  "admin",                  default: false
+    t.string   "profile_picture"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
